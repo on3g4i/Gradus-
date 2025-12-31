788a3030818bc554b56222f5f636277f539a2fdd
on3g4i <d3887269@gmail.com>
2025-12-31 03:23:31 -0300

    Atualização do arquivo de configuração do vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [ 'resources/css/app.css', 'resources/js/app.js', 'resources/js/documents.js' ],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/documents.js', 'resources/js/wordCloud.js'],
             refresh: true,
         }),
     ],
